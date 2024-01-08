e4fce94771fcfddae4212baf842495b0ce8e975d
Afrad Ahsan <afradahsan2002@gmail.com>
2024-01-08 21:52:06 +0530

    Homepage

--- lib/view/screens/main/homepage.dart
+++ lib/view/screens/main/homepage.dart
@@ -10,6 +10,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        unselectedItemColor: const Color.fromARGB(100, 255, 255, 255),
+        items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.newspaper), label: 'News'),
+        BottomNavigationBarItem(icon: Icon(Icons.newspaper), label: 'News'),
+        BottomNavigationBarItem(icon: Icon(Icons.newspaper), label: 'News')
+      ]),
+      body: SafeArea(child: Column()),
+    );
   }
 }
