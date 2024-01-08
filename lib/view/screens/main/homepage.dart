9a181390337e6663f79dcad3ebeb7c25afcef8b3
Afrad Ahsan <afradahsan2002@gmail.com>
2024-01-08 21:58:41 +0530

    edit

--- lib/view/screens/main/homepage.dart
+++ lib/view/screens/main/homepage.dart
@@ -14,14 +14,27 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.black,
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.black,
+        selectedItemColor: Colors.red,
         unselectedItemColor: const Color.fromARGB(100, 255, 255, 255),
+        elevation: 0,
         items: [
         BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: 'Home'),
         BottomNavigationBarItem(icon: Icon(Icons.newspaper), label: 'News'),
-        BottomNavigationBarItem(icon: Icon(Icons.newspaper), label: 'News'),
-        BottomNavigationBarItem(icon: Icon(Icons.newspaper), label: 'News')
+        BottomNavigationBarItem(icon: Icon(Icons.emoji_emotions_outlined), label: 'Fast Laughs'),
+        BottomNavigationBarItem(icon: Icon(Icons.download_rounded), label: 'Downloads')
       ]),
-      body: SafeArea(child: Column()),
+      body: SafeArea(child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(children: [
+              Text('For Afrad'),
+              SizedBox(width: 150,),
+              IconButton(onPressed: (){}, icon: Icon(Icons.cast)), IconButton(onPressed: (){}, icon: Icon(Icons.search)), IconButton(onPressed: (){}, icon: Icon(Icons.person))
+            ],)
+          ],
+        ),
+      )),
     );
   }
 }
